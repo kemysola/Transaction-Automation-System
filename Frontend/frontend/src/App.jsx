b27bd2e2fb79e1f7416e10e251fa5f7490e483be
kemysola <dakemilola@gmail.com>
2022-01-19 14:06:53 +0100

    Auth Components

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Main from './Components/Auth/MainPage/Main'
+
+
+export default function App() {
+   
+    return (
+        <React.Fragment>
+            <Main/>
+        </React.Fragment>
+    );
+}
--- Frontend/frontend/src/Components/Auth/Login/UserForm.jsx
+++ Frontend/frontend/src/Components/Auth/Login/UserForm.jsx
@@ -1,13 +1,61 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { makeStyles } from '@material-ui/core/styles';
+import { Form, Button, InputGroup, FormControl,ButtonGroup} from 'react-bootstrap';
 
+
+
+const useStyles = makeStyles({
+    formBg:{
+        'background': 'white',
+        'borderRadius': '13px',
+        'padding': '20px',
+    },
+    center :{
+        'textAlign':''
+    },
+    green:{
+        'background': 'green',
+        'color':'white',
+        'width': '20vw',
+        'border' : '1px transaparent green',
+        'borderRadius' :'6px',
+    },
+    
+})
 
 const UserForm =()=>{
-    return(
+    const forms = useStyles()
+      return (
         <React.Fragment>
-            <div className='container'></div>
+            <div className={forms.formBg}>
+                <Form action='' post=''>
+                    <h3>Sign In</h3>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control size="sm" type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Password</Form.Label>
+                         <Form.Control size="sm" type="password" placeholder="password" />
+                    </Form.Group>
+                  
+                       <Form.Group>
+                           <input type='checkbox'/>
+                           <small> Remember me</small>
+                       </Form.Group>
+                   
+                    
+                    <br/>
+                    <Form.Group className="mb-3 text-light" controlId="exampleForm.ControlInput1">
+                         <Form.Control size="sm" type="Button" placeholder="Login" className='bg-success' />
+                    </Form.Group>
+                
+                </Form>
+                
+            </div> 
         </React.Fragment>
-
-    )
+      );
 }
 
 export default UserForm;
