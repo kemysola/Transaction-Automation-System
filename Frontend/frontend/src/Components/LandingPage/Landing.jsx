0896d2aae4e8c396f69a7f27f054d700fe6df938
kemysola <dakemilola@gmail.com>
2022-01-20 12:43:20 +0100

    landing page

--- Frontend/frontend/src/Components/LandingPage/Landing.jsx
+++ Frontend/frontend/src/Components/LandingPage/Landing.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import {Container,Row,Col} from 'react-bootstrap';
+import MainCards from './MainCards';
+import Sidenav from './Sidenav';
+import styled from 'styled-components';
+import SubCards from './SubCards';
+
+const LandingWrapper = styled.div`
+  background: #eff1f1;
+  height:98vh;
+  margin:0;
+  padding: 0 10px;
+
+`;
 
 
 const Landing =()=>{
     return(
-        <React.Fragment></React.Fragment>
+        <React.Fragment>
+            <LandingWrapper>
+            <Row>
+                    <Col sm={3} style={{padding:'10px'}}>
+                        <Sidenav/>
+                    </Col>
+                    <Col sm={6}> 
+                        <MainCards/> 
+                    </Col>
+                    <Col sm={2}>  
+                    </Col>
+                </Row>
+            </LandingWrapper>
+                
+        </React.Fragment>
 
     )
 }
--- Frontend/frontend/src/Components/LandingPage/Sidenav.jsx
+++ Frontend/frontend/src/Components/LandingPage/Sidenav.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Row, Col, Container,Navbar, Nav, NavItem, Button, Stack} from 'react-bootstrap';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  padding: 1.5rem;
+  background-position: 150px -150px;
+  height: 80vh;
+  border-right: 1px solid black;
+  background: #eff1f1;
+
+`;
 
 
-const SideNav =()=>{
+const Sidenav =()=>{
     return(
-        <React.Fragment></React.Fragment>
+        <React.Fragment>
+            <AppWrapper>
+                <div>
+                    <Stack gap={4}>
+                        <div>
+                            <small>Transactions</small>
+                        </div>
+                        <div>
+                            <div>
+                               <small>All Transactions</small>
+                            </div>
+                            <div>
+                                <small>New Transaction</small>
+                            </div>
+                        </div>
+                        <div>
+                            <small>Staff</small>
+                        </div>
+                            <div>
+                            <small>Dashboard</small>
+                            </div>
+                            
+                    </Stack>
+                </div>
+               
 
+
+            </AppWrapper>
+        </React.Fragment>
     )
+    
 }
-
-export default SideNav;
+export default Sidenav;
+    
