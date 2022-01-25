2ef369b34b3f4733d28d46ee1d32a58808fdd0b5
Shelah <shelahameli@gmail.com>
2022-01-25 17:56:29 +0100

    Local changes

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StaffView from './Components/Staffs/StaffView';
+import Landing from './Components/LandingPage/Landing';
+import Main from './Components/Auth/MainPage/Main';
+import UpdateStaffs from './Components/Staffs/UpdateStaffs';
+import TransactionCards from './Components/Transactions/TransactionCards';
+import NewTransactions from './Components/Transactions/NewTransactions';
 
 
 export default function App() {
    
     return (
         <React.Fragment>
-            <StaffView/>
-            
+            {/* <StaffView /> */}
+            {/* <Landing /> */}
+            {/* <Main /> */}
+            {/* <UpdateStaffs /> */}
+            {/* <TransactionCards /> */}
+            <NewTransactions />
             
         </React.Fragment>
     );
