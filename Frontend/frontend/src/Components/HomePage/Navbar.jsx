0f421b21278557e12a0eb973a196a5206d5299b3
dev <dev@gmail.com>
2022-03-24 09:29:52 +0100

    dashboard and non admin page

---
+++ Frontend/frontend/src/Components/HomePage/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container, Nav, Form } from 'react-bootstrap';
+import { FaBars } from 'react-icons/fa';
+import InfraCredit from '../../Images/i.png';
+import { Link } from 'react-router-dom';
+import MenuOption from './MenuOption';
+import { GlobalFilter } from '../Transactions/TransactionTable';
+
+
+const NavbarNav = styled.div`
+background:#eff1f1;
+padding:1px 10px;
+margin-bottom:0.1px;
+display:flex;
+// grid-template-columns: auto auto auto auto;
+justify-content: space-between;
+align-items: center;
+`
+
+const Navbar = ({ handleToggleSidebar, props }) => {
+    return (
+        <React.Fragment>
+            <NavbarNav>
+                
+                {/*---------------------------  Logo Div ------------------------------------- */}
+                <div>
+                    <img src={InfraCredit} alt='logo' width='100' />
+                </div>
+
+                
+                {/*----------------------- Icon Div ------------------------------------------- */}
+                <div style={{ marginTop: '5px', paddingTop: '20px' , }}>
+                    <MenuOption/>
+                </div>
+
+            </NavbarNav>
+        </React.Fragment>
+    )
+}
+
+export default Navbar;
