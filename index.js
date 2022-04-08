3d92c488463ddddba25c62615fef04deafcf4546
Taiwo Akinde <oladeleakinde@gmail.com>
2022-04-08 09:50:23 +0100

    updated the backend dir

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors"); //Review
 const path = require("path")
-const bodyParser = require("body-parser")
 
 // Routes
 const authRoute = require("./backend/routes/auth");
@@ -14,27 +13,22 @@ const passwdReset = require("./backend/routes/password_reset");
 const configRoute = require("./backend/routes/configuration");
 
 dotenv.config();
-const app_port = process.env.APP_SERVER_PORT;
+// const app_port = process.env.APP_SERVER_PORT;
+const app_port = process.env.PORT;
 
 // Middlewares
 app.use(cors()); //Review need in project
 
 app.use(express.json()); //To get access to client's request.body as json object
-app.use(bodyParser.json());
 
 //to use the build during the production.
 
-//app.use(express.static(process.cwd()+"./Frontend/frontend/build/"));
-
-app.use(express.static(path.join(__dirname, './frontend/frontend/build')));
 
 
 
+app.set('port', app_port  || 3000)
 
-
-
-
-
+// Implemeting all endpoints
 try {
     app.use("/api/v1/auth", authRoute)
     app.use("/api/v1/staff", staffRegRoute)
@@ -47,21 +41,27 @@ try {
     console.error(err)
 }
 
+// if(process.env.NODE_ENV === 'production'){
+//     // app.use(express.static('Frontend/frontend/build'))
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+//     app.get("*", (req,res) =>{
+//         res.sendFile(path.resolve(__dirname,"frontend","build", "index.html"))
+    
+//     })
 
+
+// }
+app.use(express.static(path.join(__dirname, './frontend/build')));
+    app.get("*", (req,res) =>{
+        res.sendFile(path.resolve(__dirname,"frontend","build", "index.html"))
+    
+    })
 
 process.on('unhandledRejection', (reason, promise) => {
     console.log('Unhandled Rejection at:', reason.stack || reason)
 });
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './frontend/frontend/build/index.html'));
-  });
-
-/*app.get('*', (req,res) => {
-    res.sendFile(process.cwd()+"./Frontend/frontend/build/index.html");
-  });*/
-
-app.listen(app_port || 5001, () => {
-    console.log(`InfraCreditTRS Server Listening on Port: ${app_port}`)
+app.listen(app_port || 3000, () => {
+    console.log(`InfraCreditTRS Server Listening on Port: ${app.get('port')}`)
 });
 
