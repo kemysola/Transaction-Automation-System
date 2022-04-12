35ea80d1f8418659cf10c4dc8cf7d34d814f71fa
Taiwo Akinde <oladeleakinde@gmail.com>
2022-04-12 09:47:53 +0100

    cleaned up the index.js in backend - removed comments

--- index.js
+++ index.js
@@ -1,73 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const dotenv = require("dotenv");
-// const cors = require("cors"); //Review
-// const path = require("path")
-// const bodyParser = require("body-parser")
-
-// // Routes
-// const authRoute = require("./backend/routes/auth");
-// const staffRegRoute = require("./backend/routes/staff");
-// const transactionRoute = require("./backend/routes/transaction");
-// const reportsRoute = require("./backend/routes/reporting");
-// const passwdReset = require("./backend/routes/password_reset");
-// const configRoute = require("./backend/routes/configuration");
-
-// dotenv.config();
-// // const app_port = process.env.APP_SERVER_PORT;
-// const app_port = process.env.PORT;
-
-// // Middlewares
-// app.use(cors()); //Review need in project
-
-// app.use(express.json()); //To get access to client's request.body as json object
-// app.use(bodyParser.json());
-
-// //to use the build during the production.
-
-// //app.use(express.static(process.cwd()+"./Frontend/frontend/build/"));
-
-// app.use(express.static(path.join(__dirname, './frontend/frontend/build')));
-
-
-
-
-
-
-
-
-// try {
-//     app.use("/api/v1/auth", authRoute)
-//     app.use("/api/v1/staff", staffRegRoute)
-//     app.use("/api/v1/transaction", transactionRoute)
-//     app.use("/api/v1/report", reportsRoute)
-//     app.use("/api/v1/password_reset", passwdReset)
-//     app.use("/api/v1/configuration", configRoute)
-
-// } catch (err) {
-//     console.error(err)
-// }
-
-
-
-// process.on('unhandledRejection', (reason, promise) => {
-//     console.log('Unhandled Rejection at:', reason.stack || reason)
-// });
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './frontend/frontend/build/index.html'));
-// });
-
-// /*app.get('*', (req,res) => {
-//     res.sendFile(process.cwd()+"./Frontend/frontend/build/index.html");
-//   });*/
-
-// app.listen(app_port || 3000, () => {
-//     console.log(`InfraCreditTRS Server Listening on Port: ${app_port}`)
-// });
-
-
-
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
@@ -85,11 +15,11 @@ const configRoute = require("./backend/routes/configuration");
 dotenv.config();
 // const app_port = process.env.APP_SERVER_PORT;
 const app_port = process.env.PORT;
+
 // Middlewares
 app.use(cors()); //Review need in project
 app.use(express.json()); //To get access to client's request.body as json object
 //to use the build during the production.
-//app.use(express.static(process.cwd()+"./Frontend/frontend/build/"));
 app.use(express.static(path.join(__dirname, './frontend/frontend/build')));
 
 
@@ -97,13 +27,6 @@ app.set('port', app_port  || 3000)
 // Implemeting all endpoints
 
 //app.use(express.static(path.join(__dirname, './frontend/frontend/build')));
-
-
-
-
-
-
-
 
 try {
     app.use("/api/v1/auth", authRoute)
@@ -154,73 +77,3 @@ app.listen(app_port || 3000, () => {
     console.log(`InfraCreditTRS Server Listening on Port: ${app.get('port')}`)
 
 });
-
-
-
-// const express = require("express");
-// const app = express();
-// const dotenv = require("dotenv");
-// const cors = require("cors"); //Review
-// const path = require("path")
-
-// // Routes
-// const authRoute = require("./backend/routes/auth");
-// const staffRegRoute = require("./backend/routes/staff");
-// const transactionRoute = require("./backend/routes/transaction");
-// const reportsRoute = require("./backend/routes/reporting");
-// const passwdReset = require("./backend/routes/password_reset");
-// const configRoute = require("./backend/routes/configuration");
-
-// dotenv.config();
-// // const app_port = process.env.APP_SERVER_PORT;
-// const app_port = process.env.PORT;
-
-// // Middlewares
-// app.use(cors()); //Review need in project
-
-// app.use(express.json()); //To get access to client's request.body as json object
-
-// //to use the build during the production.
-
-
-
-
-// app.set('port', app_port  || 3000)
-
-// // Implemeting all endpoints
-// try {
-//     app.use("/api/v1/auth", authRoute)
-//     app.use("/api/v1/staff", staffRegRoute)
-//     app.use("/api/v1/transaction", transactionRoute)
-//     app.use("/api/v1/report", reportsRoute)
-//     app.use("/api/v1/password_reset", passwdReset)
-//     app.use("/api/v1/configuration", configRoute)
-    
-// } catch (err) {
-//     console.error(err)
-// }
-
-// // if(process.env.NODE_ENV === 'production'){
-// //     // app.use(express.static('Frontend/frontend/build'))
-// //     app.use(express.static(path.join(__dirname, '../frontend/build')));
-// //     app.get("*", (req,res) =>{
-// //         res.sendFile(path.resolve(__dirname,"frontend","build", "index.html"))
-    
-// //     })
-
-
-// // }
-// app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     app.get("*", (req,res) =>{
-//         res.sendFile(path.resolve(__dirname,"../frontend","build", "index.html"))
-    
-//     })
-
-// process.on('unhandledRejection', (reason, promise) => {
-//     console.log('Unhandled Rejection at:', reason.stack || reason)
-// });
-
-// app.listen(app_port || 3000, () => {
-//     console.log(`InfraCreditTRS Server Listening on Port: ${app.get('port')}`)
-// });
-
