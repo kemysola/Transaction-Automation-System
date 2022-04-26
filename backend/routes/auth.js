6c6655bff658e701eb2cb90990c48bd198ce46d9
dev <dev@gmail.com>
2022-04-26 13:48:44 +0100

    added categorization total to the management dashboard

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -70,8 +70,8 @@ const AADParameters = {
     tenant: process.env.tenant,
     authorityHostUrl: 'https://login.windows.net',
     clientId: process.env.clientID,
-    //redirectUri: 'http://localhost:5001/api/v1/auth/app/login',
-    redirectUri: 'https://trms01-server.azurewebsites.net/api/v1/auth/app/login', 
+    redirectUri: 'http://localhost:5001/api/v1/auth/app/login',
+    //redirectUri: 'https://trms01-server.azurewebsites.net/api/v1/auth/app/login', 
     clientSecret: process.env.value
 };
 
@@ -119,8 +119,8 @@ router.get('/app/login', (req, res) => {
         const paramsString = {"user":response.account.username, "token":response.accessToken};
         let searchParams = new URLSearchParams(paramsString);
 
-        //res.redirect('http://localhost:3000/login?'+searchParams);
-        res.redirect('https://trms01-server.azurewebsites.net/login?'+searchParams);
+        res.redirect('http://localhost:3000/login?'+searchParams);
+        //res.redirect('https://trms01-server.azurewebsites.net/login?'+searchParams);
 
     }).catch((error) => {
         console.log(error);
