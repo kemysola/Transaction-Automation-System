3952a80f8eb4e2b0e018107b91a3ecb6be713105
Babalola <lbabalola@deloitte.com.ng>
2022-04-28 12:41:30 +0100

    fixed origination all staff module

--- Frontend/frontend/src/Components/Dashboard/Origination/stafflist/Dropdown.jsx
+++ Frontend/frontend/src/Components/Dashboard/Origination/stafflist/Dropdown.jsx
@@ -50,8 +50,8 @@ return(
     <Dropdown.Menu style = {{ height: '200px', overflowY: 'scroll'}}>
           <Dropdown.Item href="http://localhost:3000/org-dashboard"> All </Dropdown.Item>
           {staff.map((opt, i) => (
-            // <Dropdown.Item href={`http://trms01-server.azurewebsites.net/staff_transaction_report?${staff[i].email}`} key={staff[i].email} value={`${staff[i].firstname} {staff[i].lastname}`}>{staff[i].firstname} {staff[i].lastname}</Dropdown.Item >
-            <Dropdown.Item href={`http://localhost:3000/staff_transaction_report?${staff[i].email}`} key={staff[i].email} value={`${staff[i].firstname} {staff[i].lastname}`}>{staff[i].firstname} {staff[i].lastname}</Dropdown.Item >
+            <Dropdown.Item href={`http://trms01-server.azurewebsites.net/staff_transaction_report?${staff[i].email}`} key={staff[i].email} value={`${staff[i].firstname} {staff[i].lastname}`}>{staff[i].firstname} {staff[i].lastname}</Dropdown.Item >
+            // <Dropdown.Item href={`http://localhost:3000/staff_transaction_report?${staff[i].email}`} key={staff[i].email} value={`${staff[i].firstname} {staff[i].lastname}`}>{staff[i].firstname} {staff[i].lastname}</Dropdown.Item >
 
              ))}
       </Dropdown.Menu>
