ae722711b271d7b503722643753633020e5b1591
dev <dev@gmail.com>
2022-05-17 17:25:25 +0100

    management dashboard scope and performances

--- Frontend/frontend/src/Components/Dashboard/Management/Cards/managementCards/greenAndAnberCard.jsx
+++ Frontend/frontend/src/Components/Dashboard/Management/Cards/managementCards/greenAndAnberCard.jsx
@@ -1,7 +1,178 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Service from "../../../../../Services/Service";
+import { Row, Col, Card, Stack, Container } from "react-bootstrap";
+function GreenAndAnberCard() {
+  const [guarPipeline, setGuarPipeline] = useState([]);
+  const [actual, setActual] = useState([]);
+  const [region, setRegion] = useState([]);
+  const [product, setProduct] = useState([]);
+  const [data, setData] = useState([]);
+  const [industry, setIndustry] = useState([]);
 
-export default function greenAndAnberCard() {
+  useEffect(() => {
+    Service.getAllStaff()
+      .then((res) => {
+        //console.log(res.data.staff);
+        setData(res.data.staff);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    Service.getAllDeals()
+      .then((res) => {
+        console.log(res.data.deals);
+        setActual(res.data.deals);
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
+  }, []);
+
+  // get green and amber actual values and variances
+  var amber = actual.reduce(function (filtered, arr) {
+    if (arr.deal_category === "Yellow") {
+      var someNewValue = arr.dealsize;
+      filtered.push(someNewValue);
+    }
+    return filtered;
+  }, []);
+
+  var green = actual.reduce(function (filtered, arr) {
+    if (arr.deal_category === "Green") {
+      var someNewValue = arr.dealsize;
+      filtered.push(someNewValue);
+    }
+    return filtered;
+  }, []);
+
+  var amberTotal = amber.reduce(function (tot, arr) {
+    return tot + parseFloat(arr);
+  }, 0);
+
+  var greenTotal = green.reduce(function (tot, arr) {
+    return tot + parseFloat(arr);
+  }, 0);
+
+  var greenV = data.reduce(function (tot, arr) {
+    return tot + Number(arr.greentransaction);
+  }, 0);
+
+  var amberValue = data.reduce(function (tot, arr) {
+    return tot + Number(arr.ambertransaction);
+  }, 0);
+
+  var greenAndAmberVariance = 0;
+  //green and amber total
+  var amber = actual.reduce(function (filtered, arr) {
+    if (arr.deal_category === "Yellow") {
+      var someNewValue = arr.dealsize;
+      filtered.push(someNewValue);
+    }
+    return filtered;
+  }, []);
+
+  var green = actual.reduce(function (filtered, arr) {
+    if (arr.deal_category === "Green") {
+      var someNewValue = arr.dealsize;
+      filtered.push(someNewValue);
+    }
+    return filtered;
+  }, []);
+
+  //**green total*/
+  var amberTotal = amber.reduce(function (tot, arr) {
+    return tot + parseFloat(arr);
+  }, 0);
+
+  var greenTotal = green.reduce(function (tot, arr) {
+    return tot + parseFloat(arr);
+  }, 0);
+
+  var gandAactual = greenTotal + amberTotal;
+  let gandAtarget = greenV + amberValue;
+
+  let varianceAmount = gandAtarget - gandAactual;
+
+  function varianceDisplay(variance) {
+    if (variance < 1) {
+      let varianceAns = variance * -1;
+      return (
+        <span style={{ color: "green" }}>↑ {varianceAns.toFixed(1)}bn</span>
+      );
+    } else if (!isFinite(variance) || isFinite(variance)) {
+      return ` - `;
+    }
+
+    return <span style={{ color: "red" }}>↓ {variance.toFixed(1)}bn </span>;
+  }
+
+  let variancePercent = ((varianceAmount / gandAtarget) * 100).toFixed(1);
+
+  function variancePerDisplay(variancePer) {
+    if (variancePer < 1) {
+      let varianceAns = variancePer * -1;
+      return <span style={{ color: "green" }}>↑ {varianceAns}% </span>;
+    } else if (!isFinite(variancePer) || isFinite(variancePer)) {
+      return ` - `;
+    }
+    return <span style={{ color: "red" }}>↓ {variancePer}% </span>;
+  }
+
   return (
-    <div>greenAndAnberCard</div>
-  )
+    <React.Fragment>
+      <Card style={{ padding: "10px" , background:'white' }}>
+        <Row className='my-1'>
+          <small style={{ fontSize: "13px" , fontWeight:'bold'}}>GREEN & AMBER DEALS</small>
+          <br/>
+          <br/>
+          <Col sm={4} style={{ borderRight:'2px solid white'}}>
+            <Stack gap={0} className="d-flex justify-content-center">
+              {greenTotal + amberTotal}
+              <br/>
+              <small
+                style={{ fontSize: "11px", color: "blue", fontWeight: "bold" }}
+                className=" mt-3"
+              >
+                <br/>
+                ACTUAL
+              </small>
+              <br />
+            </Stack>
+          </Col>
+          <Col sm={4}>
+            <Stack gap={0} className="d-flex justify-content-center">
+              {greenV + amberValue}
+              <br />
+              <small
+                style={{ fontSize: "11px", color: "black", fontWeight: "bold" }}
+                className="mt-2"
+              >
+                <br/>
+                <br />
+                TARGET
+              </small>
+            </Stack>
+          </Col>
+          <Col sm={4}>
+            <Stack gap={0}>
+              <small style={{ fontSize: "17px" }}>
+                {variancePerDisplay(variancePercent)}
+              </small>
+              <small className="mb-3">{varianceDisplay(varianceAmount)}</small>
+              <small
+                style={{ fontSize: "11px", color: "red", fontWeight: "bold" }}
+              >
+                VARIANCE
+              </small>
+            </Stack>
+          </Col>
+        </Row>
+      </Card>
+    </React.Fragment>
+  );
 }
+
+export default GreenAndAnberCard;
--- Frontend/frontend/src/Components/Home.jsx
+++ Frontend/frontend/src/Components/Home.jsx
@@ -3,8 +3,8 @@ import React, { useEffect } from 'react';
 export default function Home() {
 
     useEffect(() => {
-    window.location.assign('https://trms01-server.azurewebsites.net/api/v1/auth')
-        //window.location.assign('http://localhost:5001/api/v1/auth')
+   window.location.assign('https://trms01-server.azurewebsites.net/api/v1/auth')
+        // window.location.assign('http://localhost:5001/api/v1/auth')
 
     }, []);
 
