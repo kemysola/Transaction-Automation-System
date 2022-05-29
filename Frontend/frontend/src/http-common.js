e42fe42fd33868a90a5239fd8b72da1706f7fd4d
dev <dev@gmail.com>
2022-05-29 19:41:28 +0100

    fixed the infinity issue with the cards on management

--- Frontend/frontend/src/http-common.js
+++ Frontend/frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: "https://trms01-server.azurewebsites.net/api/v1/",
+  baseURL: "https://trms01-server.azurewebsites.net/api/v1/",
     // baseURL: "http://localhost:5001/api/v1/",
   headers: {
     token: `Bearer ${localStorage.getItem('token')}`,
