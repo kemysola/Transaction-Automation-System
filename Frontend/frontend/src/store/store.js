98f914829584b206d66d4277803d57ef9f43aee0
Awomolo <aawomolo@deloitte.com.ng>
2022-09-07 06:59:29 +0100

    added service workers , created decrpt script to encrypt data, created dotenv files to protect the endpoints

--- Frontend/frontend/src/store/store.js
+++ Frontend/frontend/src/store/store.js
@@ -1,16 +1,11 @@
-import { setupListeners } from '@reduxjs/toolkit/query'
 import { configureStore } from '@reduxjs/toolkit'
 import { apiServices } from '../Services/apiServices'
 export const store = configureStore({
-    reducer: {
-      // Add the generated reducer as a specific top-level slice
-      [apiServices.reducerPath]: apiServices.reducer,
-    },
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
-    middleware: (getDefaultMiddleware) =>
+  reducer: {
+      [apiServices.reducerPath]: apiServices.reducer
+  },
+  middleware: getDefaultMiddleware =>
       getDefaultMiddleware().concat(apiServices.middleware),
-  })
-
-  setupListeners(store.dispatch)
+  devTools: true
+})
 
---
+++ Frontend/frontend/src/utils/decryptData.js
@@ -0,0 +1,60 @@
+import * as aesjs from 'aes-js';
+
+function hexToBase64(hexstring) {
+  return btoa(
+    hexstring
+      .match(/\w{2}/g)
+      .map(function (a) {
+        return String.fromCharCode(parseInt(a, 16));
+      })
+      .join('')
+  );
+}
+
+function base64ToHex(str) {
+  const hex = [];
+  const bin = atob(str.replace(/[ \r\n]+$/, ''));
+  for (let i = 0; i < bin.length; i += 1) {
+    let tmp = bin.charCodeAt(i).toString(16);
+    if (tmp.length === 1) {
+      tmp = '0' + tmp;
+    }
+    hex.push(tmp);
+  }
+  return hex.join('');
+}
+
+export const encryptData = (data) => {
+  const iv = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const key = aesjs.utils.utf8.toBytes(process.env.REACT_APP_ENCRYPTION_KEY || '');
+
+  const text = JSON.stringify(data);
+  const textBytes = aesjs.utils.utf8.toBytes(text);
+
+  const aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
+  const encryptedBytes = aesCbc.encrypt(aesjs.padding.pkcs7.pad(textBytes));
+
+  const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+  const encryptedBase64 = hexToBase64(encryptedHex);
+  const user_data3 = { encryptedData: encryptedBase64 };
+
+  return user_data3;
+};
+
+export function decryptData(data) {
+  try {
+    const iv = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const key = aesjs.utils.utf8.toBytes('6B58703273357638792F423F4528472B');
+
+    // console.log({ data: atob(data), replaced: base64ToHex(data) });
+    const encryptedBytes = aesjs.utils.hex.toBytes(base64ToHex(data));
+
+    const aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
+    const decryptedBytes = aesjs.padding.pkcs7.strip(aesCbc.decrypt(encryptedBytes));
+    const decrytpedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+
+    return JSON.parse(decrytpedText) ;
+  } catch (error) {
+    console.log({ error });
+  }
+}
