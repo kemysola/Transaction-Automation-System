3b194d5f39d34e01d7a0ccadcdc7cbbfd596f7cb
Awomolo <aawomolo@deloitte.com.ng>
2022-09-20 07:54:36 +0100

    configured redux toolkit store and added authorization header to apiservices

--- Frontend/frontend/src/Services/apiServices.js
+++ Frontend/frontend/src/Services/apiServices.js
@@ -1,45 +1,23 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import axios from '../http-common'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
 export const apiServices = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ axios}),
-    tagTypes: ['Report'],
-    endpoints: (builder) => ({
-        getReport: builder.query({
-            query: () => 'transaction/all_deals',
-            // transformResponse: res => res.sort((a, b) => b.id - a.id),
-            providesTags: ['Report']
-        }),
-        addReport: builder.mutation({
-            query: (report) => ({
-                url: '/report',
-                method: 'POST',
-                body: report
-            }),
-            invalidatesTags: ['Report']
-        }),
-        updateReport: builder.mutation({
-            query: (report) => ({
-                url: `/report/${report.id}`,
-                method: 'PUT',
-                body: report
-            }),
-            invalidatesTags: ['Report']
-        }),
-        deleteReport: builder.mutation({
-            query: ({ id }) => ({
-                url: `/report/${id}`,
-                method: 'DELETE',
-                body: id
-            }),
-            invalidatesTags: ['Report']
-        }),
+    reducerPath:'productApi',
+    baseQuery: fetchBaseQuery(
+        {baseURL: "http://localhost:5001/api/v1/",
+        // baseURL: "https://trms01-server.azurewebsites.net/api/v1/",
+        headers: {
+            token: `Bearer ${localStorage.getItem('token')}`,
+            'Content-type': 'application/json; charset=utf-8',
+          }
+    }),
+    endpoints:(builder)=> ({
+        getAlReport:builder.query({
+            query:() =>'report'
+        })
+
     })
 })
 
-export const {
-    useGetReportQuery,
-    useAddReportMutation,
-    useUpdateReportMutation,
-    useDeleteReportMutation
-} = apiServices
+
+
+export const {useGetAllReportQuery} = apiServices;
